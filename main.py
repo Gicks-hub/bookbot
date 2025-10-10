f330f6c70f6ac2760a64b66f37c7aeeb8e5791b0
github_username_here <email@example.com>
2025-10-10 06:45:01 +0200

    starting to use git finally: added all files

---
+++ main.py
@@ -0,0 +1,6 @@
+from stats import get_num_words
+from stats import fit_character_in_dict
+from stats import num_char_dictionary
+
+
+num_char_dictionary(fit_character_in_dict())
---
+++ stats.py
@@ -0,0 +1,46 @@
+def get_book_text():
+	with open("books/frankenstein.txt") as f:
+		file_contents =f.read()
+	return file_contents
+
+def main():
+	text = get_book_text().split()
+	return text
+
+def count_words_in_text(searched_word):
+	text = main()
+	count = 0
+	for word in text:
+		if word == searched_word:
+			count+=1
+	return f"Found {count} total words"
+
+def get_num_words():
+	text = main()
+	count=0
+	for word in text:
+		count+=1
+	print(f"Found {count} total words")
+
+def fit_character_in_dict():
+	book_character = get_book_text().lower()
+	alphabet_dict={}
+	for character in book_character:
+		if character in alphabet_dict:
+			alphabet_dict[character] +=1
+		else:
+			alphabet_dict[character] = 1
+	print(alphabet_dict)
+	return alphabet_dict
+
+def sort_dict_by_count(dict):
+	return dict["num"]
+
+def num_char_dictionary(dict):
+	num_char_dict={}
+	for element in dict:
+		if element.isalpha():
+			num_char_dict["char"] = element
+			num_char_dict["num"] = dict[element]
+			print(num_char_dict)
+	return num_char_dict
