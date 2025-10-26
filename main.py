0ce37980e447a6920c4722d3c35354ae3132b274
github_username_here <email@example.com>
2025-10-26 07:07:53 +0100

    Added new books

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from stats import get_num_words
 from stats import fit_character_in_dict
 from stats import num_char_dictionary
 from stats import print_records
+import sys
+
+
 
 print_records(num_char_dictionary(fit_character_in_dict()))
+print(sys.argv[1])
--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
-def get_book_text():
-	with open("books/frankenstein.txt") as f:
+def get_book_text(book):
+	with open(book) as f:
 		file_contents =f.read()
 	return file_contents
 
